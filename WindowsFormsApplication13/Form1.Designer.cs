c1a66b88e2230eaf20f8fe443ab566d0ea8a1801
zygas3 <zygimantas.pagalys@gmail.com>
2016-01-24 09:27:24 +0200

    First time

---
+++ WindowsFormsApplication13/Form1.Designer.cs
@@ -0,0 +1,417 @@
+﻿namespace WindowsFormsApplication13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Išlaidos = new System.Windows.Forms.Label();
+            this.Maistas = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Degalai = new System.Windows.Forms.Label();
+            this.TextButton2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Vanduo = new System.Windows.Forms.Label();
+            this.Elektra = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Šildymas = new System.Windows.Forms.Label();
+            this.Dujos = new System.Windows.Forms.Label();
+            this.Kita = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.Drabužiai = new System.Windows.Forms.Label();
+            this.Vaikųbūreliai = new System.Windows.Forms.Label();
+            this.Kitą = new System.Windows.Forms.Label();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.Viso = new System.Windows.Forms.Label();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Išlaidos
+            // 
+            this.Išlaidos.AutoSize = true;
+            this.Išlaidos.Location = new System.Drawing.Point(264, 9);
+            this.Išlaidos.Name = "Išlaidos";
+            this.Išlaidos.Size = new System.Drawing.Size(42, 13);
+            this.Išlaidos.TabIndex = 7;
+            this.Išlaidos.Text = "Išlaidos";
+            this.Išlaidos.Click += new System.EventHandler(this.Išlaidos_Click);
+            // 
+            // Maistas
+            // 
+            this.Maistas.AutoSize = true;
+            this.Maistas.Location = new System.Drawing.Point(125, 156);
+            this.Maistas.Name = "Maistas";
+            this.Maistas.Size = new System.Drawing.Size(43, 13);
+            this.Maistas.TabIndex = 10;
+            this.Maistas.Text = "Maistas";
+            this.Maistas.Click += new System.EventHandler(this.Maistas_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(125, 212);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Paskola";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Degalai
+            // 
+            this.Degalai.AutoSize = true;
+            this.Degalai.Location = new System.Drawing.Point(125, 186);
+            this.Degalai.Name = "Degalai";
+            this.Degalai.Size = new System.Drawing.Size(43, 13);
+            this.Degalai.TabIndex = 12;
+            this.Degalai.Text = "Degalai";
+            // 
+            // TextButton2
+            // 
+            this.TextButton2.Location = new System.Drawing.Point(242, 122);
+            this.TextButton2.Name = "TextButton2";
+            this.TextButton2.Size = new System.Drawing.Size(100, 20);
+            this.TextButton2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(242, 72);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(348, 72);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(136, 122);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 4;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(136, 72);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(348, 122);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(239, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Komunalinės paslaugos";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // Vanduo
+            // 
+            this.Vanduo.AutoSize = true;
+            this.Vanduo.Location = new System.Drawing.Point(167, 53);
+            this.Vanduo.Name = "Vanduo";
+            this.Vanduo.Size = new System.Drawing.Size(44, 13);
+            this.Vanduo.TabIndex = 13;
+            this.Vanduo.Text = "Vanduo";
+            // 
+            // Elektra
+            // 
+            this.Elektra.AutoSize = true;
+            this.Elektra.Location = new System.Drawing.Point(276, 53);
+            this.Elektra.Name = "Elektra";
+            this.Elektra.Size = new System.Drawing.Size(40, 13);
+            this.Elektra.TabIndex = 14;
+            this.Elektra.Text = "Elektra";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(345, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Internetas/Kabelinė";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Šildymas
+            // 
+            this.Šildymas.AutoSize = true;
+            this.Šildymas.Location = new System.Drawing.Point(163, 99);
+            this.Šildymas.Name = "Šildymas";
+            this.Šildymas.Size = new System.Drawing.Size(48, 13);
+            this.Šildymas.TabIndex = 16;
+            this.Šildymas.Text = "Šildymas";
+            this.Šildymas.Click += new System.EventHandler(this.Šildymas_Click);
+            // 
+            // Dujos
+            // 
+            this.Dujos.AutoSize = true;
+            this.Dujos.Location = new System.Drawing.Point(272, 99);
+            this.Dujos.Name = "Dujos";
+            this.Dujos.Size = new System.Drawing.Size(34, 13);
+            this.Dujos.TabIndex = 17;
+            this.Dujos.Text = "Dujos";
+            // 
+            // Kita
+            // 
+            this.Kita.AutoSize = true;
+            this.Kita.Location = new System.Drawing.Point(383, 99);
+            this.Kita.Name = "Kita";
+            this.Kita.Size = new System.Drawing.Size(25, 13);
+            this.Kita.TabIndex = 18;
+            this.Kita.Text = "Kita";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(182, 153);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 19;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(182, 209);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 20;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(182, 179);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 21;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(182, 235);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 20);
+            this.textBox10.TabIndex = 22;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(182, 261);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(100, 20);
+            this.textBox11.TabIndex = 23;
+            // 
+            // Drabužiai
+            // 
+            this.Drabužiai.AutoSize = true;
+            this.Drabužiai.Location = new System.Drawing.Point(125, 238);
+            this.Drabužiai.Name = "Drabužiai";
+            this.Drabužiai.Size = new System.Drawing.Size(51, 13);
+            this.Drabužiai.TabIndex = 24;
+            this.Drabužiai.Text = "Drabužiai";
+            this.Drabužiai.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Vaikųbūreliai
+            // 
+            this.Vaikųbūreliai.AutoSize = true;
+            this.Vaikųbūreliai.Location = new System.Drawing.Point(106, 264);
+            this.Vaikųbūreliai.Name = "Vaikųbūreliai";
+            this.Vaikųbūreliai.Size = new System.Drawing.Size(70, 13);
+            this.Vaikųbūreliai.TabIndex = 25;
+            this.Vaikųbūreliai.Text = "Vaikų bureliai";
+            // 
+            // Kitą
+            // 
+            this.Kitą.AutoSize = true;
+            this.Kitą.Location = new System.Drawing.Point(136, 291);
+            this.Kitą.Name = "Kitą";
+            this.Kitą.Size = new System.Drawing.Size(25, 13);
+            this.Kitą.TabIndex = 26;
+            this.Kitą.Text = "Kita";
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(182, 291);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(100, 20);
+            this.textBox12.TabIndex = 27;
+            // 
+            // Viso
+            // 
+            this.Viso.AutoSize = true;
+            this.Viso.Location = new System.Drawing.Point(136, 320);
+            this.Viso.Name = "Viso";
+            this.Viso.Size = new System.Drawing.Size(27, 13);
+            this.Viso.TabIndex = 28;
+            this.Viso.Text = "Viso";
+            this.Viso.Click += new System.EventHandler(this.Viso_Click);
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(182, 317);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(100, 20);
+            this.textBox13.TabIndex = 29;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 30;
+            this.label3.Text = "Balansas";
+            // 
+            // textBox14
+            // 
+            this.textBox14.Location = new System.Drawing.Point(12, 69);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.Size = new System.Drawing.Size(100, 20);
+            this.textBox14.TabIndex = 31;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(404, 198);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(197, 21);
+            this.comboBox1.TabIndex = 32;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(450, 287);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 33;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(718, 357);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox14);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.Viso);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.Kitą);
+            this.Controls.Add(this.Vaikųbūreliai);
+            this.Controls.Add(this.Drabužiai);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.Kita);
+            this.Controls.Add(this.Dujos);
+            this.Controls.Add(this.Šildymas);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Elektra);
+            this.Controls.Add(this.Vanduo);
+            this.Controls.Add(this.Degalai);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Maistas);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Išlaidos);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.TextButton2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Išlaidos;
+        private System.Windows.Forms.Label Maistas;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Degalai;
+        private System.Windows.Forms.TextBox TextButton2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Vanduo;
+        private System.Windows.Forms.Label Elektra;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label Šildymas;
+        private System.Windows.Forms.Label Dujos;
+        private System.Windows.Forms.Label Kita;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.Label Drabužiai;
+        private System.Windows.Forms.Label Vaikųbūreliai;
+        private System.Windows.Forms.Label Kitą;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.Label Viso;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox14;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+
+
+    }
+}
+
